f86dfab73d866e2a07d81fca5d881d0bf2e46724
Dave Casinginan <davecasinginan@gmail.com>
2025-04-11 15:30:52 +0800

    created user inputs to prompt users to insert their questions and answers

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,11 @@
 #prompt user to generate questions and answers
+print('Make your desired qustion')
+question = input('Enter question: ')
+option_a = input('Enter option for a: ')
+option_b = input('Enter option for b: ')
+option_c = input('Enter option for c: ')
+option_d = input('Enter correct answer for d: ')
 #get the correct answer
+correct_answer = input('Enter the correct answer: ')
 #write the question into file
 #ask the user if they want to continue
