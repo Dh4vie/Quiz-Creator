08615a0d5dcf4e6133a94162204aec745fcb4a17
Dave Casinginan <davecasinginan@gmail.com>
2025-04-11 15:40:43 +0800

    restricting the user in correct answer prompt to strictly the available choices only

--- quiz_creator.py
+++ quiz_creator.py
@@ -5,7 +5,10 @@ option_a = input('Enter option for a: ')
 option_b = input('Enter option for b: ')
 option_c = input('Enter option for c: ')
 option_d = input('Enter correct answer for d: ')
+
 #get the correct answer
-correct_answer = input('Enter the correct answer: ')
+while correct_answer not in ['a', 'b', 'c', 'd']:
+    correct_answer = input('Enter the correct answer: ').strip().lower()
+
 #write the question into file
 #ask the user if they want to continue
