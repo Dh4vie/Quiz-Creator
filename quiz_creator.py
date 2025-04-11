6357fb6ba5c58c8841c9bd3f8181a0feb7222c38
Dave Casinginan <davecasinginan@gmail.com>
2025-04-11 16:01:27 +0800

    set script to generate file and write the inputs to txt file

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,14 +1,24 @@
 #prompt user to generate questions and answers
-print('Make your desired qustion')
-question = input('Enter question: ')
-option_a = input('Enter option for a: ')
-option_b = input('Enter option for b: ')
-option_c = input('Enter option for c: ')
-option_d = input('Enter correct answer for d: ')
+filename = "gathered_quiz_data.txt"
+
+with open(filename, 'a') as file: #used 'a' to add the gathered data
+    while True:
+        print('Make your desired qustion')
+        question = input('Enter question: ')
+        option_a = input('Enter option for A: ')
+        option_b = input('Enter option for B: ')
+        option_c = input('Enter option for C: ')
+        option_d = input('Enter option for D: ')
 
 #get the correct answer
-while correct_answer not in ['a', 'b', 'c', 'd']:
-    correct_answer = input('Enter the correct answer: ').strip().lower()
+while correct_answer not in ['A', 'B', 'C', 'D']:
+    correct_answer = input('Enter the correct answer: ').strip().upper()
 
 #write the question into file
+file.write(f'Question: {question}\n')
+file.write(f'A) {option_a}\n')
+file.write(f'B) {option_b}\n')
+file.write(f'C) {option_c}\n')
+file.write(f'D) {option_d}\n')
+file write(f'\nCorrect Answer: {correct_answer}')
 #ask the user if they want to continue
